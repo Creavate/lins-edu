4c4ad8eee0dfb23ebd250f22e4a4d848f8e0ab29
devjf <djfcoder@outlook.com>
2025-09-07 03:04:25 -0300

    refactor: insert async functions to better performance.

--- js/login.js
+++ js/login.js
@@ -1,7 +1,7 @@
 const loginForm = $("#login-form");
 
-$(document).ready(function () {
-
+// When the login form is submitted, prevent the default form submission and redirect to the index page.
+$(() => {
   loginForm.on("submit", function (e) {
     e.preventDefault();
     window.location.href = "index.html";
